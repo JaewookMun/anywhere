b9e596cfa2ac182a43eb9924ae412c6aa3b34f9d
JaewookMun <mun1103.dev@gmail.com>
2023-04-27 00:20:05 +0900

    create how to send http request

--- src/main/java/prj/margin/anywhere/http/HttpConnection.java
+++ src/main/java/prj/margin/anywhere/http/HttpConnection.java
@@ -3,9 +3,11 @@ package prj.margin.anywhere.http;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Map;
 
 @Component
 public class HttpConnection {
@@ -15,10 +17,31 @@ public class HttpConnection {
      * {@link java.net.HttpURLConnection} is used
      * @deprecated
      */
-    public void sendRequest(String apiUrl, RequestMethod requestMethod) throws IOException {
+    public String sendRequest(String apiUrl, RequestMethod requestMethod, Map<String, String> headers, String queryStringForParams) throws IOException {
         URL url = new URL(apiUrl);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod(requestMethod.toString());
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod(requestMethod.toString());
 
+        // set parameters
+        con.setDoOutput(true);
+        DataOutputStream out = new DataOutputStream(con.getOutputStream());
+        out.writeBytes(queryStringForParams);
+        out.flush();
+        out.close();
+
+        // set header
+        for (Map.Entry<String, String> header : headers.entrySet()) {
+            con.setRequestProperty(header.getKey(), header.getValue());
+        }
+
+        int httpStatus = con.getResponseCode();
+
+        if(httpStatus == HttpURLConnection.HTTP_OK)
+            // todo: 정상호출
+            System.out.println("200");
+        else System.out.println("error"); // todo: 실패
+
+        // todo: return result
+        return null;
     }
 }
