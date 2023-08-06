b81aad1f19b26d5a9b64acd078e7a82b408532fe
JaewookMun <mun1103.dev@gmail.com>
2023-08-06 21:54:47 +0900

    Add test profile and css

--- src/test/java/prj/margin/anywhere/service/ApiServiceTest.java
+++ src/test/java/prj/margin/anywhere/service/ApiServiceTest.java
@@ -1,28 +1,31 @@
 package prj.margin.anywhere.service;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import prj.margin.anywhere.config.auth.dto.ApplicationOAuth2NaverProperties;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class ApiServiceImplTest {
+@ActiveProfiles("test")
+class ApiServiceTest {
 
     @Autowired
-    private ApiServiceImpl apiService;
+    private ApiService apiService;
     @Autowired
     private ApplicationOAuth2NaverProperties naverProperties;
 
     @Test
+    @DisplayName("테스트")
     public void test() throws IOException {
-        apiService.revokeNaverMapSearchApi();
+        String keyword = "이가식당";
+        apiService.revokeNaverMapSearchApi(keyword);
 
         // given
         // when
